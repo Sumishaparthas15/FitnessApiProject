b576395fa91f8a5b57aff47b9755d4afd62af340
Sumisha PS <sumishasudha392@gmail.com>
2025-07-07 18:45:17 +0530

    Add FitnessClass and Booking models

--- fitness_api/booking/views.py
+++ fitness_api/booking/views.py
@@ -2,5 +2,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class FitnessClassListView(APIView):
+
     def get(self, request):
-        return Response({"message": "List of fitness classes"})
+        return Response({"message": "List all fitness classes"})
